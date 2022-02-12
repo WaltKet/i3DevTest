205914c26a70d821ac9b0d2d52f9fa4baebd2618
WaltKet <60006030+WaltKet@users.noreply.github.com>
2022-02-11 20:24:25 -0800

    Free Cam Rotates on Mouse Down, and rearWheelCam

---
+++ Assets/Scripts/ActivateFreeLookCam.cs
@@ -0,0 +1,39 @@
+using Cinemachine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ActivateFreeLookCam : MonoBehaviour
+{
+    void Start()
+    {
+        CinemachineCore.GetInputAxis = GetAxisCustom;
+    }
+    public float GetAxisCustom(string axisName)
+    {
+        if (axisName == "Mouse X")
+        {
+            if (Input.GetMouseButton(0))
+            {
+                return UnityEngine.Input.GetAxis("Mouse X");
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        else if (axisName == "Mouse Y")
+        {
+            if (Input.GetMouseButton(0))
+            {
+                return UnityEngine.Input.GetAxis("Mouse Y");
+            }
+            else
+            {
+                return 0;
+            }
+        }
+        return UnityEngine.Input.GetAxis(axisName);
+    }
+}
